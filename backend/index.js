b6556a7deb851758edc9b53e7d24ad0f62224d17
Jeet Debnath <jeetdeb232@gmail.com>
2024-03-03 00:00:41 +0530

    Added BE

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,34 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
 
-app.post('/todo', (req, res) => {
+app.post('/createTodo', (req, res) => {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+
+    if (!parsedPayload.success) {
+        return res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+    }
     
+    // Put it into MongoDB
 })
 
-app.post('/todos', (req, res) => {
-
+app.get('/allTodos', (req, res) => {
+    // Get all Todos from MongoDB
 })
 
-app.post('/completed', (req, res) => {
+app.put('/completedTodo', (req, res) => {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
 
+    if(!parsedPayload.success) {
+        return res.status(411).json({
+            msg: "You sent me wrong inputs"
+        })
+    }
+    // Update the following ID Todo into MongoDB
 })
